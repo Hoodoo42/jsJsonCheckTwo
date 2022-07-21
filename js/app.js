6e2c8a4a64ba1882959e6eea97c9daa657445183
Hoodoo42 <create@hoodooverse.ca>
2022-07-21 17:39:14 -0600

    A knowledge check on saving objects into cookies by first converting to a string through JSON

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// creating an object that will be turned into string so it can be saved in a cookie
+let selection = {
+  title: `Black Candle`,
+  price: 43,
+  imageUrl: `https://images.unsplash.com/photo-1504036583833-1b9340d6722f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80`,
+  description: `The Black Candles are lovely`,
+};
+
+// setting the variable that will hold the string version of the object
+// Cookies.set is named userSelection and is holding the string version of the object (through the value of the variable selectionJSON)
+let selectionJson = JSON.stringify(selection);
+Cookies.set(`userSelection`, selectionJson);
